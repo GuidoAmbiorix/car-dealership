7184b95456d61ceb86c8adb747e4861f7c07de3e
GuidoAmbiorix <guidoambiorix@gmail.com>
2024-08-19 20:01:56 -0400

    Se termino la seccion2

--- src/cars/cars.service.ts
+++ src/cars/cars.service.ts
@@ -1,14 +1,24 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
 export class CarsService {
-  private cars = ['Toyota', 'Honda', 'Jeep'];
+  private cars = [
+    {
+      id: 1,
+      brand: 'Toyota',
+      model: 'Corolla',
+    },
+  ];
 
   findAll() {
     return this.cars;
   }
 
-  findOneById(id: string) {
-    return this.cars[id];
+  findOneById(id: number) {
+    const car = this.cars.find((car) => car.id === id);
+
+    if (!car) throw new NotFoundException(`Car with id '${id}' not found`);
+
+    return car;
   }
 }
